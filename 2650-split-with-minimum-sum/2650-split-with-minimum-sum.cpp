e7ba157541ede1bf0de5297ffcc84b24d0143722
DIWESH KUMAR TIWARI <110690043+diwesh09@users.noreply.github.com>
2023-09-08 13:09:49 +0530

    Time: 3 ms (27.41%), Space: 6 MB (77.65%) - LeetHub

--- 2650-split-with-minimum-sum/2650-split-with-minimum-sum.cpp
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int splitNum(int num) {
-        int num1 = 0, num2 = 0, x = 1;
-        vector<int> v;
-        while(num){
-            v.push_back(num%10);
-            num /= 10;
-        }
-        sort(v.begin(),v.end());
-        for(auto &i: v){
-            num *= 10;
-            num += i;
-        }
-        while(num){
-            num1 += x*(num%10);
-            num /= 10;
-            num2 += x*(num%10);
-            num /= 10;
-            x *= 10;
-        }
-        return num1+num2;
+    int splitNum(int num, string a = "", string b = "", bool alter = true) {
+
+        auto numstr = to_string(num);
+        sort(numstr.begin(), numstr.end());
+
+        for (auto c: numstr) 
+        
+        if (alter = !alter) 
+        a.push_back(c); 
+        else 
+        b.push_back(c);
+
+        return stoi(a) + stoi(b);   
     }
 };
