6a744f51efee2d182e799c26b5dfbd65642a3b17
DIWESH KUMAR TIWARI <diweshtiwari7417@gmail.com>
2023-11-10 22:43:56 +0530

    Time: 328 ms (84.24%), Space: 105.6 MB (94.85%) - LeetHub

--- 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    void dfs(vector<int> &ans,map<int,vector<int>> &g,int root,int prev){
-        ans.push_back(root);
-        for(int neigh:g[root]){
-            if(neigh!=prev){
-                dfs(ans,g,neigh,root);
-            }
-        }
-    }
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-        vector<int> ans;
-        map<int,vector<int>> g;
-        for(auto it:adjacentPairs){
-            g[it[0]].push_back(it[1]);
-            g[it[1]].push_back(it[0]);
+        
+        unordered_map<int, vector<int>> ps;
+        
+        for (auto &p : adjacentPairs) {
+            ps[p[0]].push_back(p[1]);
+            ps[p[1]].push_back(p[0]);
         }
-        int root=0;
-        for(auto it: g){
-            if(it.second.size()==1){
-                root=it.first;
+        
+        vector<int> res;
+        
+        for (auto &p : ps) {
+            if (p.second.size() == 1) {
+            
+                res.push_back(p.first);
+                res.push_back(p.second[0]);
                 break;
             }
         }
-
-        dfs(ans,g,root,INT_MAX);
-        return ans;
+      
+        
+        while (res.size() < adjacentPairs.size() + 1) {
+          
+            auto tail = res.back(), prev = res[res.size() - 2];
+            auto &next = ps[tail];
+            if (next[0] != prev)
+                res.push_back(next[0]);
+            else
+                res.push_back(next[1]);
+        }
+        
+        return res;
     }
 };
+
+
